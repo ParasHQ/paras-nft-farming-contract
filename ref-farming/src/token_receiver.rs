46c9293c2d4ff7fa970f3f18e6657a8700567c6f
joundy <hafizjoundys@gmail.com>
2022-07-25 12:33:58 +0700

    fix: typo

--- ref-farming/src/token_receiver.rs
+++ ref-farming/src/token_receiver.rs
@@ -31,13 +31,13 @@ impl FungibleTokenReceiver for Contract {
     ) -> PromiseOrValue<U128> {
         let sender: AccountId = sender_id.into();
         let amount: u128 = amount.into();
-        let nft_contract_id = env::predecessor_account_id();
+        let ft_contract_id = env::predecessor_account_id();
 
         if msg.is_empty() {
             // ****** seed Token deposit in ********
 
             // if seed not exist, it will panic
-            let seed_farm = self.get_seed(&nft_contract_id);
+            let seed_farm = self.get_seed(&ft_contract_id);
 
             assert_eq!(seed_farm.get_ref().seed_type, SeedType::FT, "Cannot deposit FT to this seed");
 
@@ -53,7 +53,7 @@ impl FungibleTokenReceiver for Contract {
             }
 
             self.internal_seed_deposit(
-                &nft_contract_id,
+                &ft_contract_id,
                 &sender,
                 amount.into(),
                 SeedType::FT,
@@ -65,7 +65,7 @@ impl FungibleTokenReceiver for Contract {
                 format!(
                     "{} deposit FT seed {} with amount {}.",
                     sender,
-                    nft_contract_id,
+                    ft_contract_id,
                     amount,
                 )
                 .as_bytes(),
