806b672918bab921fe613be68c2df5438ea47c22
joundy <hafizjoundys@gmail.com>
2022-07-29 17:38:47 +0700

    add: tsetnet test

--- test/config.js
+++ test/config.js
@@ -1,33 +1,34 @@
-const farmingContractName = 'dev-1647599503667-53369781664552'
+const farmingContractName = 'dev-1659090864519-55985752044487'
 const nftContractName = 'paras-token-v1.testnet'
 const ftContractName = 'dev-1631277489384-75412609538902'
 const ownerAccountName = 'orang.testnet'
 
 module.exports = function getConfig(network = 'mainnet') {
 	let config = {
-		networkId: "testnet",
-		nodeUrl: "https://rpc.testnet.near.org",
-		walletUrl: "https://wallet.testnet.near.org",
-		helperUrl: "https://helper.testnet.near.org",
-        farmingContractName: farmingContractName,
-        nftContractName: nftContractName,
-        ftContractName: ftContractName,
-        ownerAccountName: ownerAccountName
+		networkId: 'testnet',
+		nodeUrl: 'https://rpc.testnet.near.org',
+		walletUrl: 'https://wallet.testnet.near.org',
+		helperUrl: 'https://helper.testnet.near.org',
+		farmingContractName: farmingContractName,
+		nftContractName: nftContractName,
+		ftContractName: ftContractName,
+		ownerAccountName: ownerAccountName,
 	}
 
 	switch (network) {
-	case 'testnet':
-		config = {
-			explorerUrl: "https://explorer.testnet.near.org",
-			...config,
-			GAS: "200000000000000",
-			gas: "200000000000000",
-			gas_max: "300000000000000",
-			DEFAULT_NEW_ACCOUNT_AMOUNT: "2",
-			DEFAULT_NEW_CONTRACT_AMOUNT: "5",
-			GUESTS_ACCOUNT_SECRET: "7UVfzoKZL4WZGF98C3Ue7tmmA6QamHCiB1Wd5pkxVPAc7j6jf3HXz5Y9cR93Y68BfGDtMLQ9Q29Njw5ZtzGhPxv",
-        }
-    }
+		case 'testnet':
+			config = {
+				explorerUrl: 'https://explorer.testnet.near.org',
+				...config,
+				GAS: '200000000000000',
+				gas: '200000000000000',
+				gas_max: '300000000000000',
+				DEFAULT_NEW_ACCOUNT_AMOUNT: '2',
+				DEFAULT_NEW_CONTRACT_AMOUNT: '5',
+				GUESTS_ACCOUNT_SECRET:
+					'7UVfzoKZL4WZGF98C3Ue7tmmA6QamHCiB1Wd5pkxVPAc7j6jf3HXz5Y9cR93Y68BfGDtMLQ9Q29Njw5ZtzGhPxv',
+			}
+	}
 
 	return config
 }
--- test/test-utils.js
+++ test/test-utils.js
@@ -1,31 +1,44 @@
-const fs = require("fs");
-const BN = require('bn.js');
-const fetch = require('node-fetch');
-const nearAPI = require('near-api-js');
-const { KeyPair, Account, Contract, utils: { format: { parseNearAmount } } } = nearAPI;
-const getConfig = require('./config');
+const fs = require('fs')
+const BN = require('bn.js')
+const fetch = require('node-fetch')
+const nearAPI = require('near-api-js')
 const {
-	networkId, 
-    farmingContractName, nftContractName, ftContractName,
-    ownerAccountName,
-    contractMethods, gas, gas_max, nodeUrl, walletUrl,
-	DEFAULT_NEW_ACCOUNT_AMOUNT, 
+	KeyPair,
+	Account,
+	Contract,
+	utils: {
+		format: { parseNearAmount },
+	},
+} = nearAPI
+const getConfig = require('./config')
+const {
+	networkId,
+	farmingContractName,
+	nftContractName,
+	ftContractName,
+	ownerAccountName,
+	contractMethods,
+	gas,
+	gas_max,
+	nodeUrl,
+	walletUrl,
+	DEFAULT_NEW_ACCOUNT_AMOUNT,
 	DEFAULT_NEW_CONTRACT_AMOUNT,
 	GUESTS_ACCOUNT_SECRET,
-} = getConfig('testnet');
+} = getConfig('testnet')
 
 const keyStore = new nearAPI.keyStores.UnencryptedFileSystemKeyStore(
-    `${process.env.HOME}/.near-credentials/`
+	`${process.env.HOME}/.near-credentials/`
 )
 
 const near = new nearAPI.Near({
-    deps: {
-            keyStore: keyStore,
-    },
-    networkId: networkId,
-    keyStore: keyStore,
-    nodeUrl: nodeUrl,
-    walletUrl: walletUrl
+	deps: {
+		keyStore: keyStore,
+	},
+	networkId: networkId,
+	keyStore: keyStore,
+	nodeUrl: nodeUrl,
+	walletUrl: walletUrl,
 })
 
 const farmingContractAccount = new nearAPI.Account(near.connection, farmingContractName)
@@ -36,133 +49,150 @@ farmingContractAccount.addAccessKey = (publicKey) =>
 		publicKey,
 		farmingContractName,
 		{
-            viewMethods: ["get_seed_info", "list_user_seeds", "get_farm"],
-            changeMethods: ["new", "create_simple_farm", "storage_deposit", "force_upgrade_seed", "upgrade_lookup_map", "migrate"]
-        },
-		parseNearAmount("0.1")
-	);
+			viewMethods: ['get_seed_info', 'list_user_seeds', 'get_farm'],
+			changeMethods: [
+				'new',
+				'create_simple_farm',
+				'storage_deposit',
+				'force_upgrade_seed',
+				'upgrade_lookup_map',
+				'migrate',
+			],
+		},
+		parseNearAmount('0.1')
+	)
 
-const farmingContract = new nearAPI.Contract(
-    farmingContractAccount,
-    farmingContractName,
-    {
-      viewMethods: ["get_seed_info", "list_user_seeds", "list_user_nft_seeds", "get_farm"],
-      changeMethods: ["new", "create_simple_farm", "storage_deposit", "force_upgrade_seed", "upgrade_lookup_map", "migrate"]
-    }
-)
+const farmingContract = new nearAPI.Contract(farmingContractAccount, farmingContractName, {
+	viewMethods: [
+		'get_seed_info',
+		'list_user_seeds',
+		'list_user_nft_seeds',
+		'get_farm',
+		'list_user_locked_seeds',
+	],
+	changeMethods: [
+		'new',
+		'create_simple_farm',
+		'storage_deposit',
+		'force_upgrade_seed',
+		'upgrade_lookup_map',
+		'migrate',
+		'lock_ft_balance',
+		'unlock_ft_balance',
+	],
+})
 
-const nftContract = new nearAPI.Contract(
-    ownerAccount,
-    nftContractName,
-    {
-      viewMethods: ["nft_token"],
-      changeMethods: ["nft_transfer", "nft_transfer_call"]
-    }
-)
+const nftContract = new nearAPI.Contract(ownerAccount, nftContractName, {
+	viewMethods: ['nft_token'],
+	changeMethods: ['nft_transfer', 'nft_transfer_call'],
+})
 
-const ftContract = new nearAPI.Contract(
-    ownerAccount,
-    ftContractName,
-    {
-      viewMethods: ["ft_balance_of"],
-      changeMethods: ["ft_transfer", "ft_transfer_call", "storage_deposit"]
-    }
-)
+const ftContract = new nearAPI.Contract(ownerAccount, ftContractName, {
+	viewMethods: ['ft_balance_of'],
+	changeMethods: ['ft_transfer', 'ft_transfer_call', 'storage_deposit'],
+})
 
 async function initContract() {
 	try {
 		await farmingContract.new({
-            owner_id: farmingContractName
-        })
+			owner_id: farmingContractName,
+		})
 	} catch (e) {
-        throw e;
+		throw e
 	}
-	return { farmingContract, farmingContractNAme };
+	return { farmingContract, farmingContractNAme }
 }
 
-const getAccountBalance = async (accountId) => (new nearAPI.Account(connection, accountId)).getAccountBalance();
+const getAccountBalance = async (accountId) =>
+	new nearAPI.Account(connection, accountId).getAccountBalance()
 
-const initAccount = async(accountId, secret) => {
-	account = new nearAPI.Account(connection, accountId);
-	const newKeyPair = KeyPair.fromString(secret);
-	keyStore.setKey(networkId, accountId, newKeyPair);
-	return account;
-};
+const initAccount = async (accountId, secret) => {
+	account = new nearAPI.Account(connection, accountId)
+	const newKeyPair = KeyPair.fromString(secret)
+	keyStore.setKey(networkId, accountId, newKeyPair)
+	return account
+}
 
-const createOrInitAccount = async(accountId, secret = GUESTS_ACCOUNT_SECRET, amount = DEFAULT_NEW_CONTRACT_AMOUNT) => {
-	let account;
+const createOrInitAccount = async (
+	accountId,
+	secret = GUESTS_ACCOUNT_SECRET,
+	amount = DEFAULT_NEW_CONTRACT_AMOUNT
+) => {
+	let account
 	try {
-		account = await createAccount(accountId, amount, secret);
+		account = await createAccount(accountId, amount, secret)
 	} catch (e) {
 		if (!/because it already exists/.test(e.toString())) {
-			throw e;
+			throw e
 		}
-		account = initAccount(accountId, secret);
+		account = initAccount(accountId, secret)
 	}
-	return account;
-};
+	return account
+}
 
 async function getAccount(accountId, fundingAmount = DEFAULT_NEW_ACCOUNT_AMOUNT, secret) {
-	accountId = accountId || generateUniqueSubAccount();
-	const account = new nearAPI.Account(connection, accountId);
+	accountId = accountId || generateUniqueSubAccount()
+	const account = new nearAPI.Account(connection, accountId)
 	try {
-		await account.state();
-		return account;
-	} catch(e) {
+		await account.state()
+		return account
+	} catch (e) {
 		if (!/does not exist/.test(e.toString())) {
-			throw e;
+			throw e
 		}
 	}
-	return await createAccount(accountId, fundingAmount, secret);
-};
-
+	return await createAccount(accountId, fundingAmount, secret)
+}
 
 async function getContract(account) {
 	return new Contract(account || contractAccount, contractName, {
 		...contractMethods,
-		signer: account || undefined
-	});
+		signer: account || undefined,
+	})
 }
 
-
 const createAccessKeyAccount = (key) => {
-	connection.signer.keyStore.setKey(networkId, contractName, key);
-	return new Account(connection, contractName);
-};
+	connection.signer.keyStore.setKey(networkId, contractName, key)
+	return new Account(connection, contractName)
+}
 
 function generateUniqueSubAccount() {
-	return `t${Date.now()}.${contractName}`;
+	return `t${Date.now()}.${contractName}`
 }
 
 /// internal
 async function createAccount(accountId, fundingAmount = DEFAULT_NEW_ACCOUNT_AMOUNT, secret) {
-	const contractAccount = new Account(connection, contractName);
-	const newKeyPair = secret ? KeyPair.fromString(secret) : KeyPair.fromRandom('ed25519');
-	await contractAccount.createAccount(accountId, newKeyPair.publicKey, new BN(parseNearAmount(fundingAmount)));
-	keyStore.setKey(networkId, accountId, newKeyPair);
-	return new nearAPI.Account(connection, accountId);
+	const contractAccount = new Account(connection, contractName)
+	const newKeyPair = secret ? KeyPair.fromString(secret) : KeyPair.fromRandom('ed25519')
+	await contractAccount.createAccount(
+		accountId,
+		newKeyPair.publicKey,
+		new BN(parseNearAmount(fundingAmount))
+	)
+	keyStore.setKey(networkId, accountId, newKeyPair)
+	return new nearAPI.Account(connection, accountId)
 }
 
-module.exports = { 
+module.exports = {
 	near,
 	gas,
 	gas_max,
 	keyStore,
 	getContract,
 	getAccountBalance,
-    farmingContractAccount,
+	farmingContractAccount,
 	farmingContractName,
-    nftContractName,
-    ftContractName,
+	nftContractName,
+	ftContractName,
 	ownerAccountName,
-    farmingContract,
-    nftContract,
-    ftContract,
-    ownerAccount,
+	farmingContract,
+	nftContract,
+	ftContract,
+	ownerAccount,
 	contractMethods,
 	initAccount,
 	createOrInitAccount,
 	createAccessKeyAccount,
-	initContract, 
-    getAccount, 
-};
+	initContract,
+	getAccount,
+}
